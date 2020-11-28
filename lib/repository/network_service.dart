16235f9449feaecd2c6a04aa31fa63632e546a9b
gtxtreme <thakkargaurav409@gmail.com>
2020-11-29 01:13:09 +0530

    feat : Add support for filtering

--- lib/repository/network_service.dart
+++ lib/repository/network_service.dart
@@ -15,4 +15,21 @@ class NetworkRepository {
       return question;
     }).toList();
   }
+
+  Future<List<Question>> getFilteredQuestions(List<String> preferences) async {
+    var allQuestions = await getQuestionsFromApi();
+    List<Question> filteredQuestions = [];
+    for (var question in allQuestions) {
+      var difficulty = question.tags["difficulty"] as String;
+      var companyList = question.tags["company"] as List;
+      var jobType = question.tags["type"] as String;
+      preferences.forEach((pref) {
+        if (pref.toLowerCase() == difficulty.toLowerCase() ||
+            companyList.contains(pref) ||
+            jobType.toLowerCase() == pref.toLowerCase())
+          filteredQuestions.add(question);
+      });
+    }
+    return filteredQuestions;
+  }
 }
