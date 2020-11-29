ec82aee2a75de39b82145407a128645ad0c3f917
gtxtreme <thakkargaurav409@gmail.com>
2020-11-29 10:27:39 +0530

    fix: Handle case where user selects nothing

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -19,13 +19,11 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       key: scaffoldKey,
       appBar: AppBar(
-        title: Text("Questions")
-        ,
+        title: Text("Questions"),
         actions: [
           IconButton(
               icon: Icon(Icons.filter_alt_outlined),
               onPressed: () async {
-                debugPrint("Icon Filter Pressed");
                 result = await showModalBottomSheet(
                     elevation: 12.0,
                     backgroundColor: Colors.white,
@@ -39,90 +37,96 @@ class _HomePageState extends State<HomePage> {
                         contextWithScaffold: context,
                       );
                     });
-                debugPrint("Result: $result");
-                setState(() {
-
-                });
+                //debugPrint("Result: $result");
+                // Needed because user may decide not to select anything
+                if (result != null) setState(() {});
               })
         ],
       ),
-      body:
-          Column(
-            children: [
-             if( result.isNotEmpty)  Container(
-                padding: EdgeInsets.all(16.0),
-                width: double.maxFinite,
-                color: Colors.blueGrey[50],
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Flexible(
-                      flex: 4,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text("Showing Results for:",
-                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
-                          SizedBox(height: 5,),
-                          Text("${result.where((element) => true)}",textAlign: TextAlign.start,style: TextStyle(fontSize: 16, fontWeight: FontWeight.w300),)
-                        ],
-                      ),
+      body: Column(
+        children: [
+          if (result.isNotEmpty)
+            Container(
+              padding: EdgeInsets.all(16.0),
+              width: double.maxFinite,
+              color: Colors.blueGrey[50],
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    flex: 4,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text("Showing Results for:",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18)),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "${result.where((element) => true)}",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w300),
+                        )
+                      ],
                     ),
-                    Flexible(
-                      flex: 1,
-                      child: IconButton(
-                        icon: Icon(Icons.close_outlined),
-                        onPressed: () {
-                          setState(() {
-                            result = [];
-                          });
-                        },
-                      ),
-                    )
-                  ],
-                ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: IconButton(
+                      icon: Icon(Icons.close_outlined),
+                      onPressed: () {
+                        setState(() {
+                          result = [];
+                        });
+                      },
+                    ),
+                  )
+                ],
               ),
-              Expanded(
-                child: FutureBuilder(
-                  future: result.isEmpty
-                      ? networkRepository.getQuestionsFromApi()
-                      : networkRepository.getFilteredQuestions(result),
-                  builder: (context, AsyncSnapshot<List<Question>> snapshot) {
-                    //debugPrint("Snapshot Data: ${snapshot.data.runtimeType}");
-                    if (snapshot.hasData) {
-                      return ListView.builder(
-                          itemCount: snapshot.data.length,
-                          itemBuilder: (context, index) {
-                            var question = snapshot.data[index];
-                            return QuestionCard(
-                              question: question,
-                            );
-                          }
-                      );
-                      // return Center(
-                      //   child: Text("Hi"),
-                      // );
-                    } else {
-                      if (snapshot.hasError) {
-                        return Center(
-                          child: Text("${snapshot.error.toString()}"),
-                        );
-                      }
-                      else {
-                        return Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                    }
-                  },
-                ),
-              )
-            ],
+            ),
+          Expanded(
+            child: FutureBuilder(
+              future: result.isEmpty
+                  ? networkRepository.getQuestionsFromApi()
+                  : networkRepository.getFilteredQuestions(result),
+              builder: (context, AsyncSnapshot<List<Question>> snapshot) {
+                if (snapshot.hasData) {
+                  if(snapshot.data.isEmpty){
+                    return Center(
+                      child: Text("Nothing Here..."),
+                    );
+                  }
+                  else {
+                    return ListView.builder(
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (context, index) {
+                          var question = snapshot.data[index];
+                          return QuestionCard(
+                            question: question,
+                          );
+                        });
+
+                  }
+                } else {
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: Text("${snapshot.error.toString()}"),
+                    );
+                  } else {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                }
+              },
+            ),
           )
-      ,
-
-
+        ],
+      ),
 
       // This trailing comma makes auto-formatting nicer for build methods.
     );
